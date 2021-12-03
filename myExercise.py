4f872206706fbb886eedb928847db8443bb8e02d
b2210765038 <92204567+b2210765038@users.noreply.github.com>
2021-12-03 22:59:05 +0300

    Update myExercise.py

--- myExercise.py
+++ myExercise.py
@@ -3,10 +3,11 @@ with open(sys.argv[1], "r") as o:
     o_data = [i.split(":")for i in o.read().splitlines()]
 o_data = {i[0]: i[1] for i in o_data}
 
-for i in list(sys.argv[2].split(",")):
-    try:
-        assert i in o_data
-    except AssertionError:
-        print("Any record of \'%s\' not found!" % i)
-    else:
-        print("Name:", i, ", University:", o_data[i])
+try:
+    for i in list(sys.argv[2].split(",")):
+        try:
+            print("Name:", i, ", University:", o_data[i])
+        except KeyError:
+            print("Any record of \'%s\' not found!" % i)
+except IndexError:
+    print("No name entered!")
