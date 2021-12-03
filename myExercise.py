2aa7c8560b869315b826c4a8279bd61691bfffc6
b2210765038 <b2210765038@cs.hacettepe.edu.tr>
2021-12-03 15:07:29 +0300

    oops!

---
+++ myExercise.py
@@ -0,0 +1,12 @@
+import sys
+with open(sys.argv[1], "r") as o:
+    o_data = [i.split(":")for i in o.read().splitlines()]
+o_data = {i[0]: i[1] for i in o_data}
+
+for i in list(sys.argv[2].split(",")):
+    try:
+        assert i in o_data
+    except AssertionError:
+        print("User not found!")
+    else:
+        print("Name:", i, ", University:", o_data[i])
